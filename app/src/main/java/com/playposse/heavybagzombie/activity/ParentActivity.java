426b52c3d89f1f54339ff3f498e2f4381e15400a
thomasfischersm <thomas@playposse.com>
2017-02-24 15:55:12 -0800

    Added options menu and implemented menu item to e-mail feedback.

--- app/src/main/java/com/playposse/heavybagzombie/activity/ParentActivity.java
+++ app/src/main/java/com/playposse/heavybagzombie/activity/ParentActivity.java
@@ -3,11 +3,15 @@ package com.playposse.heavybagzombie.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.playposse.heavybagzombie.BagZombieApplication;
 import com.playposse.heavybagzombie.R;
+import com.playposse.heavybagzombie.util.EmailUtil;
 
 /**
  * An abstract {@link android.app.Activity} that contains the boilerplate to instantiate the support
@@ -25,6 +29,34 @@ public abstract class ParentActivity extends AppCompatActivity {
     }
 
     protected abstract int getLayoutResId();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.help_menu_item:
+                // TODO
+                return true;
+            case R.id.settings_menu_item:
+                // TODO
+                return true;
+            case R.id.send_feedback_menu_item:
+                EmailUtil.sendFeedbackAction(this);
+                return true;
+            case R.id.about_menu_item:
+                // TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onResume() {
